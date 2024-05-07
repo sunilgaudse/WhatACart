829b3734afd54497947f81faabfc4c8a77c59c0e
sunil <sunilgaudse555@gmail.com>
2024-05-07 19:04:21 +0530

    time change

--- whatacart-automation/src/test/java/com/test/LoginPageTest.java
+++ whatacart-automation/src/test/java/com/test/LoginPageTest.java
@@ -41,7 +41,7 @@ public class LoginPageTest extends BaseClass {
 		lp.loginToApplication("sunilgaudse", "Sarika@20001994");
 		// WebElement logout = driver.findElement(By.xpath("//a[@title='My Account']"));
 //		By locator = By.xpath("//a[@title='My Account']//span");
-		WebDriverWait wait = new WebDriverWait(driver, 5000);
+		WebDriverWait wait = new WebDriverWait(driver, 500);
 		// Thread.sleep(500);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='sunilgaudse']")));
 		WebElement logout = driver.findElement(By.xpath("//span[text()='sunilgaudse']"));
