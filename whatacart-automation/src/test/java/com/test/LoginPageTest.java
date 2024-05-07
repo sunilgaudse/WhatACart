bbf375c553608690da059445d5226b8859b50742
sunil <sunilgaudse555@gmail.com>
2024-05-07 20:57:57 +0530

    adding login successfull test

--- whatacart-automation/src/test/java/com/test/LoginPageTest.java
+++ whatacart-automation/src/test/java/com/test/LoginPageTest.java
@@ -36,18 +36,18 @@ public class LoginPageTest extends BaseClass {
 		lp = hp.loginBtn();
 	}
 
-	//@Test
-//	public void verifyLoginSuccessfull() throws InterruptedException {
-//		lp.loginToApplication("sunilgaudse", "Sarika@20001994");
-//		// WebElement logout = driver.findElement(By.xpath("//a[@title='My Account']"));
-////		By locator = By.xpath("//a[@title='My Account']//span");
-//		WebDriverWait wait = new WebDriverWait(driver, 500);
-//		// Thread.sleep(500);
-//		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='sunilgaudse']")));
-//		WebElement logout = driver.findElement(By.xpath("//span[text()='sunilgaudse']"));
-//		Assert.assertEquals(logout.getText(), "sunilgaudse");
-//
-//	}
+	@Test
+	public void verifyLoginSuccessfull() throws InterruptedException {
+		lp.loginToApplication("sunilgaudse", "Sarika@20001994");
+		// WebElement logout = driver.findElement(By.xpath("//a[@title='My Account']"));
+//		By locator = By.xpath("//a[@title='My Account']//span");
+		WebDriverWait wait = new WebDriverWait(driver, 500);
+		// Thread.sleep(500);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='sunilgaudse']")));
+		WebElement logout = driver.findElement(By.xpath("//span[text()='sunilgaudse']"));
+		Assert.assertEquals(logout.getText(), "sunilgaudse");
+
+	}
 
 	@Test
 	public void verifyLoginFailedOnInvalidCred() {
